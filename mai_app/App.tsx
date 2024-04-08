049b2e8ff9863c72a1794128f39602d88a582ec1
ninh3702 <ninhpro96@gmail.com>
2024-04-08 18:15:36 +0700

    first commit

---
+++ mai_app/App.tsx
@@ -0,0 +1,25 @@
+import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {LoginScreen, SplashScreen} from './src/screens';
+import AuthNavigator from './src/navigators/AuthNavigator';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  const [isShowSplash, setIsShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsShowSplash(false);
+    }, 1500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return isShowSplash ? (
+    <SplashScreen />
+  ) : (
+    <NavigationContainer><AuthNavigator /></NavigationContainer>
+  );
+};
+
+export default App;
