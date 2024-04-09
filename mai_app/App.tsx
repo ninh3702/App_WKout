0665f3b1ab3ecdae0ada84b9e74a9c0c50353644
ninh3702 <ninhpro96@gmail.com>
2024-04-09 16:45:53 +0700

    add swiper screen

--- mai_app/App.tsx
+++ mai_app/App.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {LoginScreen, SplashScreen} from './src/screens';
 import AuthNavigator from './src/navigators/AuthNavigator';
@@ -10,15 +10,26 @@ const App = () => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsShowSplash(false);
-    }, 1500);
+    }, 3000);
 
     return () => clearTimeout(timeout);
   }, []);
 
-  return isShowSplash ? (
-    <SplashScreen />
-  ) : (
-    <NavigationContainer><AuthNavigator /></NavigationContainer>
+  return (
+    <>
+      <StatusBar
+        barStyle={'dark-content'}
+        backgroundColor={'transparent'}
+        translucent
+      />
+      {isShowSplash ? (
+        <SplashScreen />
+      ) : (
+        <NavigationContainer>
+          <AuthNavigator />
+        </NavigationContainer>
+      )}
+    </>
   );
 };
 
--- mai_app/src/screens/SplashScreen.tsx
+++ mai_app/src/screens/SplashScreen.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, ImageBackground, Image, ActivityIndicator} from 'react-native';
+import React from 'react';
+import {appInfo} from '../constants/appInfos';
+import { SpaceComponent } from '../components';
+import { appColors } from '../constants/appColor';
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
-  )
-}
+      <ImageBackground
+        source={require('../assets/images/background.png')}
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        imageStyle={{flex: 1}}>
+        <Image
+          source={require('../assets/images/logo.png')}
+          style={{
+            width: appInfo.sizes.WIDTH * 0.7,
+            resizeMode: 'contain',
+          }}></Image>
+          <SpaceComponent height={16}></SpaceComponent>
+          <ActivityIndicator color={appColors.gray} size={40} />
+      </ImageBackground>
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;
